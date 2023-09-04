ed57ff95bfa8bf501793e3af4203a72dfaf76145
nastoika <nastya.gorlachyova@mail.ru@>
2023-09-04 16:05:06 +0300

    add async action creator and useAction hook

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,11 +1,28 @@
-import React from "react";
-import {useSelector} from "react-redux";
+import React, {useEffect} from "react";
+import type {} from 'redux-thunk/extend-redux';
 import {useTypedSelector} from "../hooks/useTypedSelector";
+import {fetchUsers} from "../store/action-creators/user";
+import {useAction} from "../hooks/useAction";
 
 
-export const UserList : React.FC = () => {
-    const {users, error, loading} = useTypedSelector( state => state.user) //typed hook
+export const UserList: React.FC = () => {
+    const {users, error, loading} = useTypedSelector(state => state.user);
+    const {fetchUsers} = useAction();
+
+    useEffect(()=> {
+        fetchUsers();
+    }, []);
+
+    if(loading){
+        return <h1>Loading...</h1>
+    }
+    if(error){
+        return <h1>{error}</h1>
+    }
     return (
-        <div></div>
-        );
+        <div>
+            {users.map(user =>
+                <div key={user.id}>{user.name}</div>)}
+        </div>
+    );
 };
